acaa6b4f57df1ac8aa9d18889108051052f71a13
alka077 <106584802+alka077@users.noreply.github.com>
2022-06-28 17:26:03 +0530

    Add files via upload

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -36,7 +36,7 @@ def create_app(test_config=None):
     CORS(app, resources={r"/*": {"origins": "*"}})
 
     """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
+    Use the after_request decorator to set Access-Control-Allow
     """
 
     @app.after_request
@@ -50,7 +50,6 @@ def create_app(test_config=None):
         return response
 
     """
-    @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
@@ -73,23 +72,15 @@ def create_app(test_config=None):
         }), 200
 
     """
-    @TODO:
     Create an endpoint to handle GET requests for questions,
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
     number of total questions, current category, categories.
-
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom
-    of the screen for three pages.
-    Clicking on the page numbers should update the questions.
     """
 
     @app.route('/questions', methods=['GET'])
     def get_questions():
 
-        # categories= Category.query.all()
         selection = Question.query.order_by(Question.id).all()
         current_questions = paginate_questions(request, selection)
         if len(current_questions) == 0:
@@ -109,12 +100,7 @@ def create_app(test_config=None):
         }), 200
 
     """
-    @TODO:
     Create an endpoint to DELETE question using a question ID.
-
-    TEST: When you click the trash icon next to a question,
-    the question will be removed.
-    This removal will persist in the database and when you refresh the page.
     """
 
     # working fine
@@ -140,25 +126,13 @@ def create_app(test_config=None):
             abort(422)
 
     """
-    @TODO:
     Create an endpoint to POST a new question,
     which will require the question and answer text,
     category, and difficulty score.
 
-    TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear
-    at the end of the last page
-    of the questions list in the "List" tab.
-    """
-    """
-    @TODO:
     Create a POST endpoint to get questions based on a search term.
     It should return any questions for whom the search term
     is a substring of the question.
-
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
     """
     @app.route('/questions', methods=['POST'])
     def create_question():
@@ -208,12 +182,7 @@ def create_app(test_config=None):
             print(e)
             abort(422)
     """
-    @TODO:
     Create a GET endpoint to get questions based on category.
-
-    TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
-    category to be shown.
     """
 
     # working fine
@@ -235,15 +204,10 @@ def create_app(test_config=None):
             abort(404)
 
     """
-    @TODO:
     Create a POST endpoint to get questions to play the quiz.
     This endpoint should take category and previous question parameters
     and return a random questions within the given category,
     if provided, and that is not one of the previous questions.
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
     """
 
     @app.route('/quizzes', methods=['POST'])
@@ -309,5 +273,13 @@ def create_app(test_config=None):
             "error": 400,
             "message": "bad request"
             }), 400
+
+    @app.errorhandler(500)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+            }), 500
 
     return app
